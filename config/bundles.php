8a8b26c077f353a30c720a093f864efb98de784b
Benjamin Ellis <ellis.benjamin@outlook.com>
2019-08-13 23:00:47 +0200

    bootstrap symfony project in order to test httplug, httplug-bundle and happyr_google_analytics

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,9 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Happyr\GoogleAnalyticsBundle\HappyrGoogleAnalyticsBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
 ];
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Happyr\GoogleAnalyticsBundle\Service\Tracker;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(Tracker $tracker)
+    {
+
+        $data=array(
+            'dh'=>'mydemo.com',
+            'dp'=>'/home',
+            'dt'=>'homepage',
+        );
+        $tracker->send($data, 'pageview');
+
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
